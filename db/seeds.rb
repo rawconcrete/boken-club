d64fc9865d88630919c0b748897a2f8664237849
Lio Catalan <182225969+LioCat-hub@users.noreply.github.com>
2025-02-08 14:48:46 +0900

    Activities and Location Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Users
+user1 = User.create(email: 'user1@example.com', pw: 'password')
+user2 = User.create(email: 'user2@example.com', pw: 'password')
+
+# Locations: Real places in Japan
+# Campsite Locations
+mount_fuji_campsite = Location.create(name: 'Mount Fuji Campsite', city: 'Fujiyoshida', prefecture: 'Yamanashi', details: 'A popular campsite located at the base of Mount Fuji with breathtaking views of the mountain.')
+nikko_campsite = Location.create(name: 'Nikko National Park', city: 'Nikko', prefecture: 'Tochigi', details: 'Beautiful campsite surrounded by lush forests and waterfalls.')
+
+# Hiking Locations
+hakone_hiking = Location.create(name: 'Hakone Hiking Trails', city: 'Hakone', prefecture: 'Kanagawa', details: 'Famous hiking trails around Mount Hakone, known for its hot springs and scenic views.')
+kamigamo_hiking = Location.create(name: 'Kamigamo Hiking Trails', city: 'Kyoto', prefecture: 'Kyoto', details: 'Popular hiking trails that lead to shrines and temples in the historic city of Kyoto.')
+
+# Rock Climbing Locations
+iwate_rock_climbing = Location.create(name: 'Iwate Rock Climbing', city: 'Iwate', prefecture: 'Iwate', details: 'A well-known climbing area offering a variety of routes with amazing views.')
+yamanashi_rock_climbing = Location.create(name: 'Yamanashi Rock Climbing', city: 'Kofu', prefecture: 'Yamanashi', details: 'Offers some of the best rock climbing routes in the Yamanashi prefecture with stunning mountain views.')
+
+# Adventures
+camping = Adventure.create(name: 'Camping', details: 'Enjoy the wilderness by setting up camp at the base of a mountain or in a national park.', id_location: mount_fuji_campsite.id)
+hiking = Adventure.create(name: 'Hiking', details: 'Explore the breathtaking hiking trails around Japan’s most famous mountains.', id_location: hakone_hiking.id)
+rock_climbing = Adventure.create(name: 'Rock Climbing', details: 'Challenge yourself with rock climbing routes in some of Japan’s best climbing spots.', id_location: iwate_rock_climbing.id)
