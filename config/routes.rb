b4d65e87be8dc835cde599e2d3d2fe3044cbab41
nico alavi <alavinic@gmail.com>
2025-02-08 13:36:05 +0900

    update routes after checking in with noemi.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,29 @@
+# Rails.application.routes.draw do
+#   get 'pages/home'
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   root 'pages#home'
+
+#   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+#   # Can be used by load balancers and uptime monitors to verify that the app is live.
+#   get "up" => "rails/health#show", as: :rails_health_check
+
+#   # Defines the root path route ("/")
+#   # root "posts#index"
+# end
+
 Rails.application.routes.draw do
-  get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'pages/home'
+  root 'pages#home'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :travel_plans, only: [:show]
+
+  resources :adventures, only: [:index, :show]
+
+  resources :locations, only: [:index, :show] do
+    resources :travel_plans, only: [:new, :create]
+  end
+
+  get "up" => "rails/health#show", as: :rails_health_check
 end
