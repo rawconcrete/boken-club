48181a2a91af7558b7bb02db38f35173a3b62945
Lio Catalan <182225969+LioCat-hub@users.noreply.github.com>
2025-02-08 16:29:16 +0900

    Add controllers and views for Locations, routes modified

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,13 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def search
+    @locations = Location.where("name LIKE ?", "%#{params[:query]}%")
+  end
+end
