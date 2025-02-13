f65882ea6bd310717b725256db30c80726c70d50
nico alavi <alavinic@gmail.com>
2025-02-13 23:26:30 +0900

    ensure controllers are loaded in application.js.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,15 @@
 // app/javascript/application.js
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "@popperjs/core"
-import "bootstrap"
+import "@hotwired/turbo-rails";
+import "controllers"; // ensure controllers are loaded
+import "@popperjs/core";
+import "bootstrap";
+
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-loading";
+
+window.Stimulus = Application.start();
+const context = require.context("controllers", true, /\.js$/);
+Stimulus.load(definitionsFromContext(context));
+
+console.log("✅ Stimulus Loaded Successfully!");
