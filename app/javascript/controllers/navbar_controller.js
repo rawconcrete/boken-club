d146b73e78d6f889161f3c7e73ac96ff3625e7dd
nico alavi <alavinic@gmail.com>
2025-02-14 00:11:23 +0900

    temporarily remove bootstrap to isolate issue.

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,9 +1,8 @@
-// app/javascript/controllers/navbar_controller.js
 import { Controller } from "@hotwired/stimulus"
-import * as bootstrap from "bootstrap"
 
 export default class extends Controller {
   connect() {
-    this.dropdown = new bootstrap.Dropdown(this.element.querySelector('.dropdown-toggle'))
+    // temporarily remove Bootstrap to isolate issue
+    console.log("Navbar controller connected")
   }
 }
