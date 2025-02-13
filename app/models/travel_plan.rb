55aad4c697aef78de8a8a59ce158d723a01a1b3b
nico alavi <alavinic@gmail.com>
2025-02-13 21:44:50 +0900

    add travel plan disclaimers and disclaimer adventures to travel plan model.

--- app/models/travel_plan.rb
+++ app/models/travel_plan.rb
@@ -4,6 +4,9 @@ class TravelPlan < ApplicationRecord
   has_many :locations, through: :travel_plans_locations
   has_many :travel_plans_adventures, dependent: :destroy
   has_many :adventures, through: :travel_plans_adventures
+  has_many :adventure_travel_plan_disclaimers
+  has_many :disclaimer_adventures, through: :adventure_travel_plan_disclaimers, source: :adventure
+
 
   validates :status, inclusion: { in: ['pending', 'completed', 'cancelled'], message: "%{value} is not a valid status" }, allow_nil: true
   validates :title, presence: true
