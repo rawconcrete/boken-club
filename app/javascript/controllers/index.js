ea571b804ace7f401d0ef9a4aa0f331a5ead6a0d
nico alavi <alavinic@gmail.com>
2025-02-13 23:08:29 +0900

    make sure stimulus is correctly loading controller and at debugging console log statements.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,7 @@
 // app/javascript/controllers/index.js
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "controllers/application";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+
+eagerLoadControllersFrom("controllers", application);
+console.log("Stimulus Controllers Loaded");
