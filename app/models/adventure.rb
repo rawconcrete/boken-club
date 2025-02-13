876813817ecef63ff1ea5adba86371a9a062a5b1
nico alavi <alavinic@gmail.com>
2025-02-13 21:45:30 +0900

    add travel plan disclaimers and disclaimer adventures to adventure model.

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -4,4 +4,7 @@ class Adventure < ApplicationRecord
   has_many :locations, through: :locations_adventures
   has_many :travel_plans_adventures
   has_many :travel_plans, through: :travel_plans_adventures
+
+  has_many :adventure_travel_plan_disclaimers
+  has_many :disclaimer_travel_plans, through: :adventure_travel_plan_disclaimers, source: :travel_plan
 end
