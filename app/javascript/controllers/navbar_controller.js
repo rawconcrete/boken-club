4597c70dace8c7f69bff1549afbf242f156813b6
nico alavi <alavinic@gmail.com>
2025-02-14 00:09:41 +0900

    instead of trying to destructure Dropdown directly from bootstrap, import all of bootstrap and then using bootstrap.Dropdown

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,9 +1,9 @@
 // app/javascript/controllers/navbar_controller.js
 import { Controller } from "@hotwired/stimulus"
-import { Dropdown } from "bootstrap"
+import * as bootstrap from "bootstrap"
 
 export default class extends Controller {
   connect() {
-    this.dropdown = new Dropdown(this.element.querySelector('.dropdown-toggle'))
+    this.dropdown = new bootstrap.Dropdown(this.element.querySelector('.dropdown-toggle'))
   }
 }
