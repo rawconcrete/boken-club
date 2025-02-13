7b3c3bc0eb020b383a677c154e0de72630aa2e0c
nico alavi <alavinic@gmail.com>
2025-02-13 11:08:53 +0900

    search#index action to return all adventures and locations if no query is given.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,5 +4,8 @@ class SearchController < ApplicationController
     adventures = Adventure.where("name ILIKE ?", "%#{query}%").pluck(:name)
     locations = Location.where("name ILIKE ?", "%#{query}%").pluck(:name)
     render json: adventures + locations
+
+    # if no query, return all
+    render json: query.present? ? (adventures + locations) : (Adventure.pluck(:name) + Location.pluck(:name))
   end
 end
