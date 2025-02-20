e8893ef95f0aa7d6daae39ebe9a338e6aadf176a
nico alavi <alavinic@gmail.com>
2025-02-20 20:11:25 +0900

    adjust adventure model.

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,11 +1,9 @@
 # app/models/adventure.rb
 class Adventure < ApplicationRecord
-  has_many :locations_adventures
+  has_many :locations_adventures, dependent: :destroy
   has_many :locations, through: :locations_adventures
-  has_many :travel_plans_adventures
+  has_many :travel_plans_adventures, dependent: :destroy
   has_many :travel_plans, through: :travel_plans_adventures
-
-  # for adding equipment:
-  has_many :adventure_equipment
+  has_many :adventure_equipment, dependent: :destroy
   has_many :equipment, through: :adventure_equipment
 end
