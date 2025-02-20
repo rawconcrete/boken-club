f79a15e035833145d82f45594c8b5e67e81e0a30
nico alavi <alavinic@gmail.com>
2025-02-20 20:11:46 +0900

    adjust location model.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,11 +1,9 @@
 # app/models/location.rb
 class Location < ApplicationRecord
-  has_many :locations_adventures
+  has_many :locations_adventures, dependent: :destroy
   has_many :adventures, through: :locations_adventures
-  has_many :travel_plans_locations
+  has_many :travel_plans_locations, dependent: :destroy
   has_many :travel_plans, through: :travel_plans_locations
-
-  # for equipment:
-  has_many :location_equipment
+  has_many :location_equipment, dependent: :destroy
   has_many :equipment, through: :location_equipment
 end
