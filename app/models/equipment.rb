7e019461ab4b538b66ce65fbd052006212576d6e
nico alavi <alavinic@gmail.com>
2025-02-20 20:47:55 +0900

    adjust equipment model to have many travel plans.

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -7,6 +7,8 @@ class Equipment < ApplicationRecord
   has_many :users, through: :user_equipments
   has_many :equipment_tips
   has_many :tips, through: :equipment_tips
+  has_many :travel_plans_equipment
+  has_many :travel_plans, through: :travel_plans_equipment
 
   validates :name, presence: true
   validates :description, presence: true
