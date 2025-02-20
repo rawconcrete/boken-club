e0e312b12ab4055e57bb1bbcef87fa58211aaeb2
nico alavi <alavinic@gmail.com>
2025-02-20 20:57:13 +0900

    update equipment selection stimulus controller for persistence.

--- app/javascript/equipment_selection_controller.js
+++ app/javascript/equipment_selection_controller.js
@@ -1,35 +1,28 @@
-export default class extends Controller {
-  static targets = ["checkbox", "planButton"]
-
-  connect() {
-    this.loadSelectedEquipment()
-    this.updatePlanButtonUrl()
-  }
-
-  toggle(event) {
-    const checkbox = event.target
-    const equipmentId = checkbox.dataset.equipmentId
-    const equipmentName = checkbox.dataset.equipmentName
-
-    let selectedEquipment = this.getSelectedEquipment()
-
-    if (checkbox.checked) {
-      selectedEquipment.push({ id: equipmentId, name: equipmentName })
-    } else {
-      selectedEquipment = selectedEquipment.filter(e => e.id !== equipmentId)
-    }
-
-    localStorage.setItem('selectedEquipment', JSON.stringify(selectedEquipment))
-    this.updatePlanButtonUrl()
-  }
-
-  updatePlanButtonUrl() {
-    if (this.hasPlanButtonTarget) {
-      const button = this.planButtonTarget
-      const url = new URL(button.href)
-      const selectedIds = this.getSelectedEquipment().map(e => e.id)
-      url.searchParams.set('equipment_ids', selectedIds.join(','))
-      button.href = url.toString()
-    }
-  }
-}
+<div class="card mb-4">
+  <div class="card-header">
+    <h5 class="mb-0">Equipment</h5>
+  </div>
+  <div class="card-body">
+    <div class="selected-items mb-3">
+      <% travel_plan.equipment.each do |item| %>
+        <div class="badge bg-primary p-2 m-1 d-inline-flex align-items-center">
+          <%= item.name %>
+          <button type="button" class="btn-close ms-2"
+                  data-action="click->travel-plan#removeEquipment"
+                  data-equipment-id="<%= item.id %>"></button>
+          <input type="hidden" name="travel_plan[equipment_ids][]" value="<%= item.id %>">
+        </div>
+      <% end %>
+    </div>
+    <div data-travel-plan-target="selectedEquipment"></div>
+    <% Equipment.all.each do |equipment| %>
+      <div class="form-check">
+        <%= check_box_tag "travel_plan[equipment_ids][]", equipment.id,
+            travel_plan.equipment_ids.include?(equipment.id),
+            id: "equipment_#{equipment.id}",
+            class: "form-check-input" %>
+        <%= label_tag "equipment_#{equipment.id}", equipment.name, class: "form-check-label" %>
+      </div>
+    <% end %>
+  </div>
+</div>
