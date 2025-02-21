e2ae448e2bad497cc304a0a4a2dc35b1f195fbc9
nico alavi <alavinic@gmail.com>
2025-02-21 15:08:12 +0900

    resolve issues in locations controller rb.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,10 +2,7 @@
 class LocationsController < ApplicationController
   def index
     @locations = if params[:query].present?
-      Location.where("name ILIKE ? OR city ILIKE ? OR prefecture ILIKE ? OR details ILIKE ? OR tips ILIKE ? OR warnings ILIKE ? OR adventure_name ILIKE ?",
-      "%#{params[:query]}%",
-      "%#{params[:query]}%",
-      "%#{params[:query]}%",
+      Location.where("name ILIKE ? OR city ILIKE ? OR prefecture ILIKE ? OR details ILIKE ?",
       "%#{params[:query]}%",
       "%#{params[:query]}%",
       "%#{params[:query]}%",
@@ -14,7 +11,7 @@ class LocationsController < ApplicationController
       Location.all
     end
 
-      respond_to do |format|
+    respond_to do |format|
       format.html
       format.json { render json: @locations }
       format.turbo_stream { render turbo_stream: turbo_stream.replace("locations-list", partial: "locations/list", locals: { locations: @locations }) }
@@ -31,7 +28,15 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render json: { id: @location.id, name: @location.name, city: @location.city, prefecture: @location.prefecture } }
+      format.json {
+        render json: {
+          id: @location.id,
+          name: @location.name,
+          city: @location.city,
+          prefecture: @location.prefecture,
+          equipment: @location.equipment.map { |e| { id: e.id, name: e.name } }
+        }
+      }
     end
   end
 end
