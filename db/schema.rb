8403de7d6a2cb3f267023e79ebf69deb42114b1d
nico alavi <alavinic@gmail.com>
2025-02-27 19:35:52 +0900

    saving schema work in progress to check out lio's UI/UX pr.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_02_22_083021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,8 +48,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_22_083021) do
   end
 
   create_table "location_equipments", force: :cascade do |t|
-    t.bigint "equipment_id", null: false
     t.bigint "location_id", null: false
+    t.bigint "equipment_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["equipment_id"], name: "index_location_equipments_on_equipment_id"
