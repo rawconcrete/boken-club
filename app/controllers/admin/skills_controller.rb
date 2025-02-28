b06db619e049202a3ab142fb26b082782bcf182b
nico alavi <alavinic@gmail.com>
2025-02-28 22:30:44 +0900

    create admin skills controller.

--- app/controllers/admin/skills_controller.rb
+++ app/controllers/admin/skills_controller.rb
@@ -2,7 +2,8 @@
 class Admin::SkillsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_admin
-  before_action :set_skill, only: [:edit, :update, :destroy]
+  before_action :set_skill, only: [:edit, :update, :destroy, :associations,
+                                   :update_adventure_associations, :update_location_associations]
 
   def index
     @skills = Skill.all.order(:name)
@@ -36,6 +37,50 @@ class Admin::SkillsController < ApplicationController
   def destroy
     @skill.destroy
     redirect_to admin_skills_path, notice: 'Skill was successfully deleted.'
+  end
+
+  # manage associations between skills and adventures/locations
+  def associations
+  end
+
+  def update_adventure_associations
+    # remove existing associations
+    @skill.adventure_skills.destroy_all
+
+    # create new associations based on selected adventure IDs
+    if params[:adventure_ids].present?
+      params[:adventure_ids].each do |adventure_id|
+        is_required = params[:required_adventure_ids].present? &&
+                      params[:required_adventure_ids].include?(adventure_id)
+
+        @skill.adventure_skills.create(
+          adventure_id: adventure_id,
+          is_required: is_required
+        )
+      end
+    end
+
+    redirect_to associations_admin_skill_path(@skill), notice: 'Adventure associations updated successfully.'
+  end
+
+  def update_location_associations
+    # remove existing associations
+    @skill.location_skills.destroy_all
+
+    # create new associations based on selected location IDs
+    if params[:location_ids].present?
+      params[:location_ids].each do |location_id|
+        is_required = params[:required_location_ids].present? &&
+                      params[:required_location_ids].include?(location_id)
+
+        @skill.location_skills.create(
+          location_id: location_id,
+          is_required: is_required
+        )
+      end
+    end
+
+    redirect_to associations_admin_skill_path(@skill), notice: 'Location associations updated successfully.'
   end
 
   private
