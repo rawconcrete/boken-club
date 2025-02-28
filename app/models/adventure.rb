580c7f65fb6bed679f93a603d24c6dbe950e687a
nico alavi <alavinic@gmail.com>
2025-02-28 21:50:41 +0900

    update adventure model.

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -7,4 +7,6 @@ class Adventure < ApplicationRecord
   has_many :travel_plans, through: :travel_plans_adventures
   has_many :adventure_equipments, dependent: :destroy
   has_many :equipment, through: :adventure_equipments
+  has_many :adventure_skills, dependent: :destroy
+  has_many :skills, through: :adventure_skills
 end
