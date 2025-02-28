02789b24ce6537c375bc52993689d1623bc030fe
nico alavi <alavinic@gmail.com>
2025-02-28 21:53:34 +0900

    update create location skills table.

--- db/migrate/20250228124815_create_location_skills.rb
+++ db/migrate/20250228124815_create_location_skills.rb
@@ -3,8 +3,11 @@ class CreateLocationSkills < ActiveRecord::Migration[7.1]
     create_table :location_skills do |t|
       t.references :location, null: false, foreign_key: true
       t.references :skill, null: false, foreign_key: true
-
+      t.boolean :is_required, default: false
       t.timestamps
     end
+
+    # add index to prevent duplicate associations
+    add_index :location_skills, [:location_id, :skill_id], unique: true
   end
 end
