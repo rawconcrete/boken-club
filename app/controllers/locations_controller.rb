d09c2cec2abf318d0cc581329abd9970d9834f3f
nico alavi <alavinic@gmail.com>
2025-03-01 16:36:29 +0900

    fix map marker clicking, remove map marker undefined, make map marker, adjust zoom, adjust map styling.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -15,16 +15,21 @@ class LocationsController < ApplicationController
       Location.all
     end
 
-    puts "Locations: #{@locations.inspect}" # Debugging: Check the locations
-    @markers = @locations.geocoded.map do |location|
+    # Ensure all locations have coordinates before creating markers
+    @locations.each do |location|
+      ensure_location_coordinates(location)
+    end
+
+    @markers = @locations.map do |location|
       {
+        id: location.id,
         name: location.name,
         city: location.city,
         prefecture: location.prefecture,
         lat: location.latitude,
         lng: location.longitude
       }
-    end
+    end.select { |marker| valid_coordinates?(marker[:lat], marker[:lng]) }
 
     respond_to do |format|
       format.html
@@ -44,12 +49,20 @@ class LocationsController < ApplicationController
       return
     end
 
+    # Make sure the location has valid coordinates
+    ensure_location_coordinates(@location)
+
+    # Create the marker with the location's coordinates
     @markers = [
       {
+        id: @location.id,
+        name: @location.name,
+        city: @location.city,
+        prefecture: @location.prefecture,
         lat: @location.latitude,
         lng: @location.longitude
       }
-    ]
+    ].select { |marker| valid_coordinates?(marker[:lat], marker[:lng]) }
 
     respond_to do |format|
       format.html
@@ -64,5 +77,70 @@ class LocationsController < ApplicationController
         }
       }
     end
+  end
+
+  private
+
+  # Ensure location has valid coordinates
+  def ensure_location_coordinates(location)
+    # Skip if location already has valid coordinates
+    return if valid_coordinates?(location.latitude, location.longitude)
+
+    # Use our hardcoded coordinates for known locations
+    special_locations = {
+      "Mount Fuji Fujiyoshida 5th Station" => [138.7274, 35.3960],
+      "Mount Takao" => [139.2438, 35.6252],
+      "Lake Kawaguchiko" => [138.7550, 35.5171],
+      "Shiretoko National Park" => [144.3580, 44.0772],
+      "Nikko National Park" => [139.6183, 36.7407],
+      "Ogawayama" => [138.4852, 36.0409],
+      "Mitake Valley" => [139.1333, 35.8167],
+      "Zao Mountain Range" => [140.4417, 38.1431],
+      "Mount Chokai" => [140.0247, 39.0986],
+      "Mount Gassan" => [139.9842, 38.5489],
+      "Oze National Park" => [139.2222, 36.9042],
+      "Mount Tanigawa" => [138.9300, 36.8344],
+      "Nagatoro" => [139.1094, 36.0944],
+      "Hachimantai" => [140.8540, 39.9583],
+      "Mount Tsukuba" => [140.1066, 36.2252],
+      "Himeji Castle" => [134.6939, 34.8396],
+      "Mt. Seppiko" => [134.7097, 34.8652]
+    }
+
+    # Check if we have hardcoded coordinates for this location
+    if special_locations.key?(location.name)
+      coords = special_locations[location.name]
+      location.longitude = coords[0]
+      location.latitude = coords[1]
+
+      # Save the coordinates to the database
+      location.save
+
+      Rails.logger.info("Set coordinates for #{location.name} using predefined values")
+
+    # Try to geocode if possible for other locations
+    elsif location.full_address.present?
+      Rails.logger.info("Attempting to geocode #{location.name} with address: #{location.full_address}")
+
+      # Try geocoding
+      location.geocode
+
+      # Save if geocoding was successful
+      if valid_coordinates?(location.latitude, location.longitude)
+        location.save
+        Rails.logger.info("Successfully geocoded #{location.name}")
+      else
+        Rails.logger.warn("Failed to geocode #{location.name}")
+      end
+    else
+      Rails.logger.warn("No coordinates available for #{location.name}")
+    end
+  end
+
+  # Check if coordinates are valid
+  def valid_coordinates?(lat, lng)
+    lat.present? && lng.present? &&
+    lat != 0 && lng != 0 &&
+    lat.between?(-90, 90) && lng.between?(-180, 180)
   end
 end
